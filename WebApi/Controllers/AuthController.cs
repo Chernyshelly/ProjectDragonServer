beee55c05584eb3670aa2fa7c63b5663bd94a499
Chernyshelly <andreycherni@gmail.com>
2022-04-04 01:02:25 +0300

    Auth with jwt added

---
+++ WebApi/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿namespace WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IdentityModel.Tokens.Jwt;
+    using System.Security.Claims;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.IdentityModel.Tokens;
+
+    [ApiController]
+    [Route("api/Auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly ILogger<AuthController> logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet("token/{username}")]
+        public string GetToken(string username)
+        {
+            var claims = new List<Claim> { new Claim(ClaimTypes.Name, username) };
+
+            // создаем JWT-токен
+            var jwt = new JwtSecurityToken(
+                    issuer: AuthOptions.ISSUER,
+                    audience: AuthOptions.AUDIENCE,
+                    claims: claims,
+                    expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)),
+                    signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            return new JwtSecurityTokenHandler().WriteToken(jwt);
+        }
+
+        [Authorize]
+        [HttpGet("test")]
+        public string Test()
+        {
+            return "works";
+        }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -5,12 +5,14 @@ namespace WebApi
     using CleanArchitecture.Infra.Data.Repositories;
     using Domain.Repository;
     using Infrastructure.EF;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.IdentityModel.Tokens;
     using Microsoft.OpenApi.Models;
 
     public class Startup
@@ -29,6 +31,34 @@ namespace WebApi
            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers();
+            services.AddAuthorization();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            // указывает, будет ли валидироваться издатель при валидации токена
+            ValidateIssuer = true,
+
+            // строка, представляющая издателя
+            ValidIssuer = AuthOptions.ISSUER,
+
+            // будет ли валидироваться потребитель токена
+            ValidateAudience = true,
+
+            // установка потребителя токена
+            ValidAudience = AuthOptions.AUDIENCE,
+
+            // будет ли валидироваться время существования
+            ValidateLifetime = true,
+
+            // установка ключа безопасности
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+
+            // валидация ключа безопасности
+            ValidateIssuerSigningKey = true,
+        };
+    });
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" });
@@ -50,6 +80,7 @@ namespace WebApi
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
