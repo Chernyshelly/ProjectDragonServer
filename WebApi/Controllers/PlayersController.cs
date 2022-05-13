f877d8c959dd379861593d4902f237dfa87d8dc2
Chernyshelly <andreycherni@gmail.com>
2022-05-13 17:09:00 +0300

    Added save file support on server

--- WebApi/Controllers/PlayersController.cs
+++ WebApi/Controllers/PlayersController.cs
@@ -1,10 +1,15 @@
 ﻿namespace WebApi.Controllers
 {
+    using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
+    using System.Threading.Tasks;
     using Application.DTO.Request;
     using Application.Interfaces;
     using Application.ViewModels;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
 
@@ -25,6 +30,51 @@
         public ActionResult<List<PlayerDto>> Get()
         {
             return this.Ok(_playerService.GetPlayers());
+        }
+
+        [HttpGet("leaderboard")]
+        public ActionResult<List<PlayerDto>> GetLeaderboard()
+        {
+            return this.Ok(_playerService.GetLeaderboardPlayers().Where(x => x.HighScore != new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)).OrderBy(x => x.HighScore));
+        }
+
+        [Authorize]
+        [HttpPost("save")]
+        public async Task<ActionResult> FileAccept(IFormFile file)
+        {
+            if (file != null)
+            {
+                var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads";
+
+                // создаем папку для хранения файлов
+                Directory.CreateDirectory(uploadPath);
+                string fullPath = $"{uploadPath}/{file.FileName}";
+
+                // сохраняем файл в папку uploads
+                using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                return this.Ok();
+            }
+
+            return this.NoContent();
+        }
+
+        [Authorize]
+        [HttpGet("save")]
+        public FileStreamResult GetSave()
+        {
+            var fileName = User.Identity.Name;
+            var mimeType = "application/octet-stream";
+            var uploadPath = $"{Directory.GetCurrentDirectory()}/uploads/{User.Identity.Name}";
+            var fileStream = new FileStream(uploadPath, FileMode.Open);
+
+            return new FileStreamResult(fileStream, mimeType)
+            {
+                FileDownloadName = fileName,
+            };
         }
 
         [HttpPost("register")]
