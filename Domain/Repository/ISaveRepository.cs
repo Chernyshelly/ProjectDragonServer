54ace836a52e579875874a15f249270797d0e497
Chernyshelly <andreycherni@gmail.com>
2022-05-23 16:39:31 +0300

    SaveRepositiry interface added

---
+++ Domain/Repository/ISaveRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Repository
+{
+    using Domain.Models;
+
+    public interface ISaveRepository
+    {
+        public Save GetSaveByUsername(string username);
+
+        public void NewSave(Save save);
+
+        public void DeleteSave(int id);
+
+        public void UpdateSave(int id, string saveFileName);
+    }
+}
