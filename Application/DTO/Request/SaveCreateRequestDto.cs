efbf9ff0c35dff3c14da8e979a7e6f064d660e62
Chernyshelly <andreycherni@gmail.com>
2022-05-24 16:58:10 +0300

    Data transfer objects added

---
+++ Application/DTO/Request/SaveCreateRequestDto.cs
@@ -0,0 +1,21 @@
+﻿namespace Application.DTO.Request
+{
+    using Application.Interfaces;
+    using Domain.Models;
+
+    public class SaveCreateRequestDto : IDtoMapper<Save>
+    {
+        public Player Player { get; set; }
+
+        public string SaveFileName { get; set; }
+
+        public Save ToModel()
+        {
+            return new Save
+            {
+                Player = this.Player,
+                SaveFileName = this.SaveFileName,
+            };
+        }
+    }
+}
---
+++ Application/DTO/Responce/SaveDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.DTO.Responce
+{
+    using Domain.Models;
+
+    public class SaveDto
+    {
+        public SaveDto(Save save)
+        {
+            Id = save.Id;
+            Player = save.Player;
+            SaveFileName = save.SaveFileName;
+        }
+
+        public int Id { get; set; }
+
+        public Player Player { get; set; }
+
+        public string SaveFileName { get; set; }
+    }
+}
