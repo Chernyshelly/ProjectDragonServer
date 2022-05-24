7df7ead707c9c7c8d55a95c50b96b1f38a07f375
Chernyshelly <andreycherni@gmail.com>
2022-05-24 17:45:02 +0300

    Added migration for save

---
+++ Infrastructure/Migrations/20220524144406_AddSave.cs
@@ -0,0 +1,42 @@
+﻿namespace Infrastructure.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+    using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+    public partial class AddSave : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Saves",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayerId = table.Column<int>(type: "integer", nullable: true),
+                    SaveFileName = table.Column<string>(type: "text", nullable: true),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Saves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Saves_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Saves_PlayerId",
+                table: "Saves",
+                column: "PlayerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Saves");
+        }
+    }
+}
